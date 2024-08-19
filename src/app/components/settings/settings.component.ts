d16fe1f3f380fb47b1b454f87c364d336d989eb7
WongShiFong <shifong.wong@57codebox.com>
2024-08-19 17:05:46 +0800

    setting-encrypt password

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -6,9 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
-  password = 'AyamGoreng@123';
+  password: string = '';
+  newPassword: string = '';
+  encryptedPassword: string = '';
+  encryptedNewPassword: string = '';
   passwordStrength: string = '';
   strengthBarWidth: string = '0%';
+  isShowPassword:boolean = false;
 
   ngOnInit() {
     this.checkPasswordStrength(this.password);
@@ -24,18 +28,57 @@ export class SettingsComponent implements OnInit {
     if (/[\W_]/.test(password)) strength += 1;
 
     // Calculate the width in pixels
-    const maxWidth = 320; // This should match your CSS max-width
-    let barWidth = (strength / 5) * maxWidth;
+    let barWidth = (strength / 5)*100;
 
-    // Ensure it does not exceed maxWidth
-    if (barWidth > maxWidth) barWidth = maxWidth;
-
-    this.strengthBarWidth = `${barWidth}px`;
+    this.strengthBarWidth = `${barWidth}%`;
 
     // Set the passwordStrength label
     if (strength === 0) this.passwordStrength = '';
     else if (strength <= 2) this.passwordStrength = 'Weak';
     else if (strength === 3) this.passwordStrength = 'Medium';
     else this.passwordStrength = 'Strong';
+  }
+
+  encryptPassword(password: string){
+    if(this.password.length > password.length){
+      this.password=this.password.slice(0,-1)
+    }
+    else if(this.password.length < password.length){
+      this.password += password.slice(-1);
+    }
+
+    if(!this.isShowPassword){
+      this.encryptedPassword = this.createStringBasedOnLength(password, '•')
+    }
+    else{
+      this.encryptedPassword = this.password; 
+    }
+
+    this.checkPasswordStrength(this.password);
+  }
+
+  encryptNewPassword(password: string){
+    if(this.newPassword.length > password.length){
+      this.newPassword=this.newPassword.slice(0,-1)
+    }
+    else if(this.newPassword.length < password.length){
+      this.newPassword += password.slice(-1);
+    }
+
+    if(!this.isShowPassword){
+      this.encryptedNewPassword = this.createStringBasedOnLength(password, '•')
+    }
+    else{
+      this.encryptedNewPassword = this.newPassword; 
+    }
+  }
+
+  createStringBasedOnLength(originalString: string, char: string): string {
+    return char.repeat(originalString.length);
+  }
+
+  showPassword(){
+    this.encryptPassword(this.password);
+    this.encryptNewPassword(this.newPassword);
   }
 }
